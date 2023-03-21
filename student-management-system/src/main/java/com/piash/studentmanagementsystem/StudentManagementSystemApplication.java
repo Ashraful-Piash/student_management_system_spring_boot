c561259883e34c8c03d24555b7d94ff543a6992e
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-03-21 22:40:35 +0600

    Changed

--- student-management-system/src/main/java/com/piash/studentmanagementsystem/StudentManagementSystemApplication.java
+++ student-management-system/src/main/java/com/piash/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -18,16 +18,16 @@ public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		Student student1 = new Student("Ashraful", "Haque","ashrafulpiash15@gmail.com");
-//		studentRepository.save(student1);
-//		Student student2 = new Student("Ashraful", "Piash","ashraful@gmail.com");
-//		studentRepository.save(student2);
-//		Student student3 = new Student("Mashrafe", "Mortaza","mortazq@gmail.com");
-//		studentRepository.save(student3);
-//		Student student4 = new Student("Shakib", "Hasan","sakibhasan@gmail.com");
-//		studentRepository.save(student4);
-//		Student student5 = new Student("Liton", "Nitol","liton@gmail.com");
-//		studentRepository.save(student5);
+		Student student1 = new Student("Ashraful", "Haque","ashrafulpiash15@gmail.com");
+		studentRepository.save(student1);
+		Student student2 = new Student("Ashraful", "Piash","ashraful@gmail.com");
+		studentRepository.save(student2);
+		Student student3 = new Student("Mashrafe", "Mortaza","mortazq@gmail.com");
+		studentRepository.save(student3);
+		Student student4 = new Student("Shakib", "Hasan","sakibhasan@gmail.com");
+		studentRepository.save(student4);
+		Student student5 = new Student("Liton", "Nitol","liton@gmail.com");
+		studentRepository.save(student5);
 
 	}
 }
